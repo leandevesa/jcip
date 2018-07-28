550293bef1a3a7f0cbabb4607d57877497575e77
leandevesa <leandevesa@gmail.com>
2018-07-28 20:46:11 -0300

    2.1 finished

---
+++ src/main/java/com/jcip/controller/chapter/two/README.md
@@ -0,0 +1,7 @@
+Conclusion:
+
+benchmark = ab -n 1000 -c 400 http://localhost:8080/2/1/
+
+1/crudo = count no tiene sentido
+1/atomic = count ok, tiempos apenas mas que crudo
+1/sync = count ok pero tiempos se disparan
---
+++ src/main/java/com/jcip/controller/chapter/two/ex/one/SynchronizedController.java
@@ -0,0 +1,34 @@
+package com.jcip.controller.chapter.two.ex.one;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value={"/2/1/synchronized"})
+public class SynchronizedController {
+
+	private long count = 0;
+
+	@RequestMapping(value={"/"}, method = RequestMethod.GET)
+	@ResponseBody
+	public String getCount() {
+		return Long.toString(count);
+	}
+
+	@RequestMapping(value={"/increment"}, method = RequestMethod.GET)
+	@ResponseBody
+	public synchronized String root() throws Exception {
+        TimeUnit.MILLISECONDS.sleep(15);
+        ++count;
+		return "OK!";
+	}
+
+	@RequestMapping(value={"/reset"}, method = RequestMethod.GET)
+	public synchronized void reset() {
+		count = 0;
+	}
+}
